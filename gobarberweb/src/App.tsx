4f60eade49d2de14bdd9e723e74839cdb14d94dc
Eber Lawrence Souza Golveia <eber.lawrence@datahome.corp>
2020-08-24 14:05:15 -0300

    Updating

---
+++ gobarberweb/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import GlobalStyles from './styles/global';
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+
+const App: React.FC = () => {
+  return (
+    <>
+      {/* <SignIn /> */}
+      <SignUp />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
